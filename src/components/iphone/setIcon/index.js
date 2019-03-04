8d5f57684c51ea1142345f74697c99ff2618d87f
yunpark9612 <yunpark9612@hotmail.com>
2019-03-04 15:28:07 +0000

    with icons updated

--- src/components/iphone/setIcon/index.js
+++ src/components/iphone/setIcon/index.js
@@ -1,42 +1,43 @@
-
+//animated icons downloaded from https://www.amcharts.com/free-animated-svg-weather-icons/
+//license link: https://creativecommons.org/licenses/by/4.0/
 
 module.exports.setUpIcons = (icon) =>{
 	switch (icon){
         case "01d":
-            return "assets/icons/sunny.png";
+            return "assets/amcharts_weather_icons_1-2/animated/day.svg";
             break;
         case "01n":
         case "02n":
-            return "assets/icons/moon.png";
+            return "assets/amcharts_weather_icons_1-2/animated/night.svg";
             break;
         case "02d":
         case "03d":
-            return "assets/icons/cloudy.png";
+            return "assets/amcharts_weather_icons_1-2/animated/cloudy-day-2.svg";
             break;
         case "04d":
         case "03n":
         case "04n":
-            return "assets/icons/clouds.png";
+            return "assets/amcharts_weather_icons_1-2/animated/cloudy.svg";
             break;
         case "09d":
         case "10d":
         case "09n":
         case "10n":
-            return "assets/icons/rainy.png";
+            return "assets/amcharts_weather_icons_1-2/animated/rainy-7.svg";
             break;
         case "11d":
         case "11n":
-            return "assets/icons/storm.png";
+            return "assets/amcharts_weather_icons_1-2/animated/thunder.svg";
             break;
         case "13d":
         case "13n":
-            return "assets/icons/snowy.png";
+            return "assets/amcharts_weather_icons_1-2/animated/snowy-5.svg";
             break;
         case "50d":
-            return "assets/icons/fog_d.png";
+            return "assets/amcharts_weather_icons_1-2/animated/cloudy-day-3.svg";
             break;
         case "50n":
-            return "assets/icons/fog_n.png";
+            return "assets/amcharts_weather_icons_1-2/animated/cloudy-night-3.svg";
             break;
         default:
             return "assets/icons/lock.png";
